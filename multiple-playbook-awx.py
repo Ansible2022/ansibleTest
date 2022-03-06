efe297338d615e8fb45299aaa326e4c1ab7f5ef0
naylinko1024 <naylinko1024@gmail.com>
2022-03-06 14:58:43 +0630

    update: multiple playbook

--- multiple-playbook-awx.py
+++ multiple-playbook-awx.py
@@ -6,6 +6,11 @@ import time
 from threading import Thread
 
 
+username = 'admin'
+password = 'admin'
+awx_ip = '192.168.60.234'
+
+
 def encoding_string(msg):
     message_bytes = msg.encode('ascii')
     base64_bytes = base64.b64encode(message_bytes)
@@ -13,59 +18,62 @@ def encoding_string(msg):
     return base64_message
 
 
-username = 'admin'
-password = 'admin'
-awx_ip = '192.168.0.103'
-#templateid = sys.argv[1]
-
-
 base64string = encoding_string(f'{username}:{password}')
 print(base64string)
 
-headers = {
-    'Authorization': "Basic " + base64string,
-    'Content-Type': 'application/json'
-}
 
+def run_template(target_host, templateid, filename):
 
+    url = f'http://{awx_ip}/api/v2/job_templates/{str(templateid)}/launch/'
 
-def run_template(templateid):
+    headers = {
+        'Authorization': "Basic " + base64string,
+        'Content-Type': 'application/json'
+    }
 
-    url = 'http://'+ awx_ip + '/api/v2/job_templates/'+ str(templateid) + '/launch/'
-    #print(url)
+    data = {
+        "extra_vars":
+            {
+                "target_host": target_host,
+                "file_name": filename,
+            }
+    }
 
-    # data = {
-    #     "extra_vars": {
-    #         "target_host": "server2",
-    #         "file_name": "text.txt",
-    #         "source_file": "/home/server2/sharefile/{{ file_name }}",
-    #         "dest_file": "/var/lib/awx/projects/{{ inventory_hostname }}/{{ file_name }}"
-    #     }
-    # }
-
-    response = requests.post(url, headers=headers)
-    #print(response.text)
+    response = requests.post(url, headers=headers,
+                             data=json.dumps(data), verify=False)
+    
+    resp = json.loads(response.text)
+    job_id = resp.get('id', '')
+    print(f'Job id: {job_id}')
 
 
 def template9():
-    print("template-9 is running at time: " + str(int(time.time())) + " seconds.")
-    run_template(9)
+    print("template-9(10MB.pdf) is running at time: " +
+          str(int(time.time())) + " seconds.")
+    run_template("server2", 9, "10MB.pdf")
+
 
 def template10():
-    print("template-10 is running at time: " + str(int(time.time())) + " seconds.")
-    run_template(10)
+    print("template-10(20MB.zip) is running at time: " +
+          str(int(time.time())) + " seconds.")
+    run_template("server2", 10, "20MB.zip")
+
 
 def template11():
-    print("template-11 is running at time: " + str(int(time.time())) + " seconds.")
-    run_template(11)
+    print("template-11(40MB.zip) is running at time: " +
+          str(int(time.time())) + " seconds.")
+    run_template("server2", 11, "40MB.zip")
 
 
-if __name__ == '__main__':
-    template_9  = Thread(target = template9)
-    template_10 = Thread(target = template10)
-    template_11 = Thread(target = template11)
+def main():
+    template_9 = Thread(target=template9)
+    template_10 = Thread(target=template10)
+    template_11 = Thread(target=template11)
 
     template_9.start()
     template_10.start()
     template_11.start()
 
+
+if __name__ == '__main__':
+    main()
